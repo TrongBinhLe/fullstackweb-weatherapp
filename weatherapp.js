7ac01ca06426cafbd52e503fd527ceea556c3108
Trong Binh Le <ltbinhbk2010@gmail.com>
2022-01-26 10:04:54 +0700

    modify server to send temperature and conditions

--- weatherapp.js
+++ weatherapp.js
@@ -10,6 +10,22 @@ app.get("/", (req, res) => {
     https.get(url, (response) => {
         console.log(response);
     });
+
+    https.get(url, (response) => {
+        console.log(response.statusCode);
+        response.on("data", (data) => {
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+            const weatherDescription = weatherData.weather[0].description;
+            const icon = weatherData.weather[0].icon;
+            const imageURL = "http://openweather.org/img/wn" + icon + "@2x.png";
+
+            res.write("<h1>The weather is currently " + weatherDescription + "</h1>");
+            res.write("<h2>The temperature in London is " + temp + "degree Celcius.</h2>");
+            res.write("<img src=" + imageURL + ">")
+            res.send();
+        })
+    });
     res.send("Server is up and running")
 });
 
